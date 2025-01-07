ec795712b1d5ab47271d335be513a69945bfc139
Ryan Johnson <ryanjohnson95@gmail.com>
2025-01-07 12:23:56 -0600

    Allow comparing more types

--- shared/state/state.go
+++ shared/state/state.go
@@ -3,46 +3,92 @@ package state
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
+)
+
+const (
+	Unavailable Value = "unavailable"
+	Unknown     Value = "unknown"
 )
 
 // Value represents a Home Assistant state, stored as a string and parsed on demand.
 type Value string
 
-// IsUnavailable checks if the state is "unavailable".
+// Checks if the state is "unavailable".
 func (s Value) IsUnavailable() bool {
-	return s == "unavailable"
+	return s == Unavailable
 }
 
-// IsUnknown checks if the state is "unknown".
+// Checks if the state is "unknown".
 func (s Value) IsUnknown() bool {
-	return s == "unknown"
+	return s == Unknown
 }
 
-// AsString returns the state as a string.
+// Returns the state as a string.
 func (s Value) String() string {
 	return string(s)
 }
 
-// AsNumber returns the state as a json.Number.
-func (s Value) AsNumber() json.Number {
-	return json.Number(s)
+// Attempts to convert the state to an int.
+func (s Value) Int() (int, error) {
+	i, err := s.Int64()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(i), nil
 }
 
-// AsBool attempts to interpret the state as a boolean.
+// Attempts to convert the state to an int64.
+func (s Value) Int64() (int64, error) {
+	return json.Number(s).Int64()
+}
+
+// Attempts to convert the state to a float32.
+func (s Value) Float32() (float32, error) {
+	f, err := s.Float64()
+	if err != nil {
+		return 0, err
+	}
+
+	return float32(f), nil
+}
+
+// Attempts to convert the state to a float64.
+func (s Value) Float64() (float64, error) {
+	return json.Number(s).Float64()
+}
+
+// Attempts to interpret the state as a boolean.
 // It supports common values like "on"/"off", "true"/"false", "locked"/"unlocked".
-func (s Value) AsBool() (bool, error) {
-	switch string(s) {
+func (s Value) Bool() (bool, error) {
+	return StringToBool(s.String())
+}
+
+// Attempts to interpret the state as a boolean, falling back to the provided default boolean.
+// It supports common values like "on"/"off", "true"/"false", "locked"/"unlocked", "open"/"closed".
+func (s Value) BoolDefault(defaultBool bool) bool {
+	b, err := StringToBool(s.String())
+	if err != nil {
+		return defaultBool
+	}
+
+	return b
+}
+
+// Attempts to parse the state as a `time.Time` using the provided layout.
+func (s Value) AsTime(layout string) (time.Time, error) {
+	return time.Parse(layout, string(s))
+}
+
+func StringToBool(state string) (bool, error) {
+	switch strings.ToLower(state) {
 	case "on", "true", "locked", "open":
 		return true, nil
 	case "off", "false", "unlocked", "closed":
 		return false, nil
 	default:
-		return false, fmt.Errorf("state is not a boolean: %s", s)
+		return false, fmt.Errorf("state is not a boolean: %s", state)
 	}
-}
-
-// AsTime attempts to parse the state as a `time.Time` using the provided layout.
-func (s Value) AsTime(layout string) (time.Time, error) {
-	return time.Parse(layout, string(s))
 }
--- shared/utils/comparator/comparator.go
+++ shared/utils/comparator/comparator.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"fmt"
 	"reflect"
+	"strconv"
 
 	"github.com/ryanjohnsontv/go-homeassistant/shared/state"
 )
@@ -28,76 +29,86 @@ const (
 	ConditionNotIn
 )
 
-func Compare(condition Condition, state state.Value) (bool, error) {
+func Compare(condition Condition, state any) (bool, error) {
 	switch v := condition.Value.(type) {
 	case string:
-		return compare(condition.Type, v, state.String())
-	case int, int8, int16, int32, int64, uint8, uint16, uint32, uintptr:
-		intState, err := state.AsNumber().Int64()
+		stringState, err := ToString(state)
 		if err != nil {
 			return false, err
 		}
 
-		intVal, err := toInt64(v)
+		return compare(condition.Type, v, stringState)
+	case int, int8, int16, int32, int64, uint8, uint16, uint32, uintptr:
+		intState, err := ToInt64(state)
+		if err != nil {
+			return false, err
+		}
+
+		intVal, err := ToInt64(v)
 		if err != nil {
 			return false, err
 		}
 
 		return compare(condition.Type, intState, intVal)
 	case float32, float64:
-		floatState, err := state.AsNumber().Float64()
+		floatState, err := ToFloat64(state)
 		if err != nil {
 			return false, err
 		}
 
-		floatVal, err := toFloat64(v)
+		floatVal, err := ToFloat64(v)
 		if err != nil {
 			return false, err
 		}
 
 		return compare(condition.Type, floatState, floatVal)
 	case bool:
-		boolState, err := state.AsBool()
+		boolState, err := ToBool(state)
 		if err != nil {
 			return false, err
 		}
 
 		return compareBool(condition.Type, boolState, v)
 	case []string:
-		return compareSlice(condition.Type, state.String(), v)
-	case []int, []int8, []int16, []int32, []uint, []uint8, []uint16, []uint32, []uint64, []uintptr:
-		intState, err := state.AsNumber().Int64()
+		stringState, err := ToString(state)
 		if err != nil {
 			return false, err
 		}
 
-		intSlice, err := toInt64Slice(v)
+		return compareSlice(condition.Type, stringState, v)
+	case []int, []int8, []int16, []int32, []uint, []uint8, []uint16, []uint32, []uint64, []uintptr:
+		intState, err := ToInt64(state)
+		if err != nil {
+			return false, err
+		}
+
+		intSlice, err := ToInt64Slice(v)
 		if err != nil {
 			return false, err
 		}
 
 		return compareSlice(condition.Type, intState, intSlice)
 	case []int64:
-		intState, err := state.AsNumber().Int64()
+		intState, err := ToInt64(state)
 		if err != nil {
 			return false, err
 		}
 
 		return compareSlice(condition.Type, intState, v)
 	case []float32:
-		floatState, err := state.AsNumber().Float64()
+		floatState, err := ToFloat64(state)
 		if err != nil {
 			return false, err
 		}
 
-		floatSlice, err := toFloat64Slice(v)
+		floatSlice, err := ToFloat64Slice(v)
 		if err != nil {
 			return false, err
 		}
 
 		return compareSlice(condition.Type, floatState, floatSlice)
 	case []float64:
-		floatState, err := state.AsNumber().Float64()
+		floatState, err := ToFloat64(state)
 		if err != nil {
 			return false, err
 		}
@@ -159,7 +170,35 @@ func compareSlice[T comparable](conditionalType ConditionType, state T, values [
 	}
 }
 
-func toInt64(value any) (int64, error) {
+type Bool interface {
+	Bool() (bool, error)
+}
+
+func ToBool(value any) (bool, error) {
+	switch v := value.(type) {
+	case bool:
+		return v, nil
+	case state.Value:
+		return v.Bool()
+	case string:
+		return state.StringToBool(v)
+	case Bool:
+		return v.Bool()
+	default:
+		val := reflect.ValueOf(value)
+		if val.Kind() == reflect.String {
+			return state.StringToBool(val.String())
+		}
+
+		return false, fmt.Errorf("type %T is not convertible to bool", value)
+	}
+}
+
+type Int interface {
+	Int64() (int64, error)
+}
+
+func ToInt64(value any) (int64, error) {
 	switch v := value.(type) {
 	case int:
 		return int64(v), nil
@@ -179,24 +218,84 @@ func toInt64(value any) (int64, error) {
 		return int64(v), nil
 	case uintptr:
 		return int64(v), nil
+	case state.Value:
+		return v.Int64()
+	case string:
+		return parseInt64(v)
+	case Int:
+		return v.Int64()
 	default:
+		val := reflect.ValueOf(value)
+		if val.Kind() == reflect.String {
+			return parseInt64(val.String())
+		}
+
 		return 0, fmt.Errorf("type %T is not convertible to int64: %v", v, v)
 	}
 }
 
-func toFloat64(value any) (float64, error) {
+func parseInt64(s string) (int64, error) {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+type Float interface {
+	Float64() (float64, error)
+}
+
+func ToFloat64(value any) (float64, error) {
 	switch v := value.(type) {
 	case float32:
 		return float64(v), nil
 	case float64:
 		return v, nil
+	case state.Value:
+		return v.Float64()
+	case string:
+		return parseFloat64(v)
+	case Float:
+		return v.Float64()
 	default:
+		val := reflect.ValueOf(value)
+		if val.Kind() == reflect.String {
+			return parseFloat64(val.String())
+		}
+
 		return 0, fmt.Errorf("type %T is not convertible to float64: %v", v, v)
 	}
 }
 
+func parseFloat64(s string) (float64, error) {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return f, nil
+}
+
+func ToString(value any) (string, error) {
+	switch v := value.(type) {
+	case string:
+		return v, nil
+	case state.Value:
+		return v.String(), nil
+	default:
+		val := reflect.ValueOf(value)
+		if val.Kind() == reflect.String {
+			return val.String(), nil
+		}
+
+		return "", fmt.Errorf("type %T is not convertible to int64: %v", value, value)
+	}
+}
+
 // Converts a generic slice of integers to []int64
-func toInt64Slice(slice any) ([]int64, error) {
+func ToInt64Slice(slice any) ([]int64, error) {
 	value := reflect.ValueOf(slice)
 	if value.Kind() != reflect.Slice {
 		return nil, fmt.Errorf("value is not a slice: %T", slice)
@@ -207,7 +306,7 @@ func toInt64Slice(slice any) ([]int64, error) {
 	for i := 0; i < value.Len(); i++ {
 		elem := value.Index(i).Interface()
 
-		intVal, err := toInt64(elem)
+		intVal, err := ToInt64(elem)
 		if err != nil {
 			return nil, fmt.Errorf("element %d is not convertible to int64: %v", i, elem)
 		}
@@ -219,7 +318,7 @@ func toInt64Slice(slice any) ([]int64, error) {
 }
 
 // Converts a generic slice of floats to []float64
-func toFloat64Slice(slice any) ([]float64, error) {
+func ToFloat64Slice(slice any) ([]float64, error) {
 	value := reflect.ValueOf(slice)
 	if value.Kind() != reflect.Slice {
 		return nil, fmt.Errorf("value is not a slice: %T", slice)
@@ -230,7 +329,7 @@ func toFloat64Slice(slice any) ([]float64, error) {
 	for i := 0; i < value.Len(); i++ {
 		elem := value.Index(i).Interface()
 
-		floatVal, err := toFloat64(elem)
+		floatVal, err := ToFloat64(elem)
 		if err != nil {
 			return nil, fmt.Errorf("element %d is not convertible to float64: %v", i, elem)
 		}
